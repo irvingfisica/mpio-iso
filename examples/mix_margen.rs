298d34fb2ba45a83c02c2242cc8de771a1c29d58
Irving Morales <irvingfisica@gmail.com>
2022-03-31 10:32:32 -0600

    aliens

--- examples/mix_margen.rs
+++ examples/mix_margen.rs
@@ -1,13 +1,12 @@
 
 use mpio_iso::isocron;
 use mpls::{datos::Sucursal};
-use serde::{Deserialize,Serialize};
 
 use std::collections::BTreeMap;
 use std::fs;
 use fs::File;
 use std::io::Write;
-use isocron::Isocrona;
+use isocron::{Isocrona,Contexto};
 
 fn main() {
 
@@ -60,10 +59,3 @@ fn main() {
     write!(salida, "{}", j).unwrap();
     
 }
-
-#[derive(Deserialize, Serialize, Debug, Clone)]
-pub struct Contexto {
-    sucursal: Sucursal,
-    isocronas: BTreeMap<String, Isocrona>
-}
-
--- src/isocron.rs
+++ src/isocron.rs
@@ -7,7 +7,8 @@ use mpls::datos::Sucursal;
 pub struct Isocrona {
     pub id: String,
     pub economico: BTreeMap<String,u32>,
-    pub demografico: BTreeMap<String,f64>
+    pub demografico: BTreeMap<String,f64>,
+    pub mpls_cercanos: Option<i32>,
 }
 
 impl Isocrona {
@@ -16,12 +17,19 @@ impl Isocrona {
             id: id.to_string(),
             economico: BTreeMap::new(),
             demografico: BTreeMap::new(),
+            mpls_cercanos: None,
         }
     }
 }
 
 #[derive(Deserialize, Serialize, Debug, Clone)]
+pub struct Alien {
+    #[serde(alias = "Suc MPLS Externas")]
+    pub mpls_cercanos: i32,
+}
+
+#[derive(Deserialize, Serialize, Debug, Clone)]
 pub struct Contexto {
     pub sucursal: Sucursal,
-    pub isocronas: BTreeMap<String, Isocrona>
+    pub isocronas: BTreeMap<String, Isocrona>,
 }
